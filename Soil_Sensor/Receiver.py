c222f7346c308d685041dd21e7257260f834e323
glcaptain00 <60938173+glcaptain00@users.noreply.github.com>
2021-11-29 22:26:21 -0600

    Updated receiver to send commands to transmitter
    Receiver sends commands to the transmitter to have the transmitter perform specific actions (i.e. toggle watering or measure soil)

--- Soil_Sensor/Receiver.py
+++ Soil_Sensor/Receiver.py
@@ -1,3 +1,7 @@
+import keydata
+import boto3
+import json
+
 # Import Python System Libraries
 import os
 import time 
@@ -13,6 +17,9 @@ import board
 import adafruit_ssd1306
 # Import RFM9x
 import adafruit_rfm9x
+
+#boto3 iot-core data client
+iot_data_client = boto3.client('iot-data', region_name=keydata.region_name, aws_access_key_id=keydata.access_key_ID, aws_secret_access_key=keydata.secret_access_key)
 
 # Button A
 btnA = DigitalInOut(board.D5)
@@ -52,7 +59,7 @@ prev_packet = None
 start = time.time()
 
 # Time to run
-PERIOD_OF_TIME = 20
+PACKET_TIMEOUT = 0.5 #seconds
 
 temptime = datetime.utcnow()
 filename = "/home/pi/logs/{:0>2d}-{:0>2d}-{:0>2d}_{:0>2d}-{:0>2d}_data_log.csv".format(temptime.month,temptime.day, temptime.year, temptime.hour, temptime.minute)
@@ -60,20 +67,45 @@ file = open(filename, "a")
 i=0
 if os.stat(filename).st_size == 0:
         file.write("Time,RSSI (dBm),Latitude,Longitude\n")
+
 no_packet_start_time = time.time()
+shadow_check_time = time.time()
+shadow_water = "false"
 
 while True:
-    
+    if time.time() > shadow_check_time + 60:
+        thingShadow = iot_data_client.get_thing_shadow(thingName=keydata.thingName, shadowName=keydata.shadowName)['payload'].read()
+        shadowData = json.loads(thingShadow.decode("utf-8"))
+        if not shadow_water == shadowData['state']['desired']['water']:
+            packDatW = {}
+            packDatW['command'] = "water"
+            packDatW['active'] = shadow_water
+            rfm9x.send(json.dumps(packDatW))
+            rfm9x.receive(timeout=5)
+            if not packet is None:
+                shadow_water = shadowData['state']['desired']['water']
+
+
+
     packet = None
     # draw a box to clear the image
     display.fill(0)
     display.text('RasPi LoRa', 35, 0, 1)
 
+    #send command to measure soil
+    packDatM = {}
+    packDatM['command'] = "measure"
+    rfm9x.send(json.dumps(packDatM))
+
     # check for packet rx
-    packet = rfm9x.receive()
-    if packet is None:
+    packet = rfm9x.receive(timeout=PACKET_TIMEOUT)
+    if packet is None: #If the timeout is reached
         display.show()
-        display.text('- Waiting for PKT -', 15, 20, 1)
+        display.text('- PKT LOSS -', 35, 20, 1)
+        print("Packet Loss")
+        file.write("{},{}\n".format(datetime.now(), "Packet Loss"))
+        no_packet_start_time = time.time()
+
     else:
         # Display the packet text and rssi
         display.fill(0)
@@ -103,21 +135,18 @@ while True:
                 #i=i+1
                 now = datetime.utcnow()
                 #file.write(str(now)+","+str(i)+","+str(-i)+","+str(i-10)+","+str(i+5)+","+str(i*i)+"\n")
-                file.write("{},{},{},{}\n".format(str(now), received_power, gps.lastLat, gps.lastLong))
+                file.write("{},{}\n".format(str(now), received_power))
                 file.flush()
     
     
     
 
     if  packet==None and time.time()>no_packet_start_time+0.5:
-        file.write("{},{},{},{}\n".format(str(datetime.now()), "Packet Loss", gps.lastLat, gps.lastLong))
+        file.write("{},{}\n".format(str(datetime.now()), "Packet Loss"))
         file.flush()
         no_packet_start_time=time.time()
     display.show()
     #time.sleep(0.1)
-    #If time has exceeded total runtime
-    #if time.time() > start + PERIOD_OF_TIME : break
-
 
     #If buttonA is pressed, then exit the while loop.
     #   This will replace the previous if statement to exit based on time.
@@ -132,8 +161,6 @@ while True:
 
 
 file.close()
-gps.running = False
-gps.join()
 display.fill(0)
 display.text("Exited...", 15, 20, 1)
 display.show()
