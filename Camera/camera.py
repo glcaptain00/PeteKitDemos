0c273216b9c022c79e993deeed4869d4c6096aa3
glcaptain00 <60938173+glcaptain00@users.noreply.github.com>
2021-12-10 22:54:20 -0600

    Added DynamoDB backup

--- Camera/camera.py
+++ Camera/camera.py
@@ -1,6 +1,13 @@
 from digitalio import DigitalInOut, Direction, Pull
 import cv2
 import board
+import boto3
+import datetime
+
+client = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
+region_name='us-west-2'
+table = dynamodb.table("TestTable")
 
 # Button A
 btnA = DigitalInOut(board.D5)
@@ -18,6 +25,9 @@ while True:
     if (btnA.value == False):
         ret, image = cam.read()
         cv2.imwrite('/home/pi/testimage.jpg', image)
+        f = open('/home/pi/testimage.jpg', 'rb')
+        table.put_item({'TestPartitionKey': 'Doorbell1', 'Time': datetime.now.ctime(), 'ImageData': f.read()})
+        f.close()
     if (btnB.value == False):
         cam.release()
         break
